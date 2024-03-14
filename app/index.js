959f029de3f2bc55c02a2098684c59ee18163e2e
matheusgomes062 <matheusgomes062@gmail.com>
2024-03-14 17:39:21 -0300

    feat: play audio  when clicking the button

--- app/index.js
+++ app/index.js
@@ -10,26 +10,14 @@ const Home = () => {
     const router = useRouter();
 
     const items = [
-        { id: 1, title: 'Item 1', iconLeft: 'heart', iconRight: 'play' },
-        { id: 2, title: 'Item 2', iconLeft: 'heart', iconRight: 'play' },
-        { id: 3, title: 'Item 3', iconLeft: 'heart', iconRight: 'play' },
-        { id: 4, title: 'Item 4', iconLeft: 'heart', iconRight: 'play'},
-        { id: 5, title: 'Item 5', iconLeft: 'heart', iconRight: 'play'},
-        { id: 6, title: 'Item 6', iconLeft: 'heart', iconRight: 'play'},
-        { id: 7, title: 'Item 7', iconLeft: 'heart', iconRight: 'play'},
-        { id: 8, title: 'Item 8', iconLeft: 'heart', iconRight: 'play'},
-        { id: 9, title: 'Item 9', iconLeft: 'heart', iconRight: 'play'},
-        { id: 10, title: 'Item 10', iconLeft: 'heart', iconRight: 'play'},
-        { id: 11, title: 'Item 11', iconLeft: 'heart', iconRight: 'play'},
-        { id: 12, title: 'Item 12', iconLeft: 'heart', iconRight: 'play'},
-        { id: 13, title: 'Item 13', iconLeft: 'heart', iconRight: 'play'},
-        { id: 14, title: 'Item 14', iconLeft: 'heart', iconRight: 'play'},
-        { id: 15, title: 'Item 15', iconLeft: 'heart', iconRight: 'play'},
-        { id: 16, title: 'Item 16', iconLeft: 'heart', iconRight: 'play'},
-        { id: 17, title: 'Item 17', iconLeft: 'heart', iconRight: 'play'},
-        { id: 18, title: 'Item 18', iconLeft: 'heart', iconRight: 'play'},
-        { id: 19, title: 'Item 19', iconLeft: 'heart', iconRight: 'play'},
-        { id: 20, title: 'Item 20', iconLeft: 'heart', iconRight: 'play'},
+        { id: 1, title: 'Item 1', iconLeft: 'heart', iconRight: 'play', group: 'ausculta_pulmonar', audio: 'Crepitaçoes Finais'},
+        { id: 2, title: 'Item 2', iconLeft: 'heart', iconRight: 'play', group: 'ausculta_pulmonar', audio: 'Crepitaçoes Grosseiras'},
+        { id: 3, title: 'Item 3', iconLeft: 'heart', iconRight: 'play', group: 'ausculta_pulmonar', audio: 'Esfregar Pleural'},
+        { id: 4, title: 'Item 4', iconLeft: 'heart', iconRight: 'play', group: 'ausculta_pulmonar', audio: 'Respiração Broncovascular'},
+        { id: 5, title: 'Item 5', iconLeft: 'heart', iconRight: 'play', group: 'ausculta_pulmonar', audio: 'Respiração Bronquica'},
+        { id: 6, title: 'Item 6', iconLeft: 'heart', iconRight: 'play', group: 'ausculta_pulmonar', audio: 'Respiração Vesicular'},
+        { id: 7, title: 'Item 7', iconLeft: 'heart', iconRight: 'play', group: 'ausculta_pulmonar', audio: 'Sibilos'},
+        { id: 8, title: 'Item 8', iconLeft: 'heart', iconRight: 'play', group: 'ausculta_pulmonar', audio: 'Sons de Respiração Reduzida'},
     ];
 
     return (
--- components/ItemList/ItemList.jsx
+++ components/ItemList/ItemList.jsx
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, Button } from 'react-native';
 import { FontAwesome6, AntDesign } from '@expo/vector-icons';
+import { Audio } from 'expo-av';
 
 const ItemList = ({ items }) => {
 
@@ -9,6 +10,43 @@ const ItemList = ({ items }) => {
     const handleItemClick = (itemId) => {
         setSelectedItemId(itemId);
     };
+
+    const [sound, setSound] = useState();
+
+    async function playSound(item) {
+        console.log('Loading Sound');
+        console.log(item.group);
+        console.log(item.audio);
+        const audioPaths = {
+            'ausculta_pulmonar_Crepitaçoes Finais': require('../../assets/audios/ausculta_pulmonar/Crepitaçoes Finais.mp3'),
+            'ausculta_pulmonar_Crepitaçoes Grosseiras': require('../../assets/audios/ausculta_pulmonar/Crepitaçoes Grosseiras.mp3'),
+            'ausculta_pulmonar_Esfregar Pleural': require('../../assets/audios/ausculta_pulmonar/Esfregar Pleural.mp3'),
+            'ausculta_pulmonar_Respiração Broncovascular': require('../../assets/audios/ausculta_pulmonar/Respiração Broncovascular.mp3'),
+            'ausculta_pulmonar_Respiração Bronquica': require('../../assets/audios/ausculta_pulmonar/Respiração Bronquica.mp3'),
+            'ausculta_pulmonar_Respiração Vesicular': require('../../assets/audios/ausculta_pulmonar/Respiração Vesicular.mp3'),
+            'ausculta_pulmonar_Sibilos': require('../../assets/audios/ausculta_pulmonar/Sibilos.mp3'),
+            'ausculta_pulmonar_Sons de Respiração Reduzida': require('../../assets/audios/ausculta_pulmonar/Sons de Respiração Reduzida.mp3'),
+        };
+
+        const audioPath = audioPaths[`${item.group}_${item.audio}`];
+
+        const { sound } = await Audio.Sound.createAsync(audioPath);
+
+        console.log('Setting Sound');
+        setSound(sound);
+
+        console.log('Playing Sound');
+        await sound.playAsync();
+    }
+
+    useEffect(() => {
+        return sound
+        ? () => {
+            console.log('Unloading Sound');
+            sound.unloadAsync();
+            }
+        : undefined;
+    }, [sound]);
 
     return (
         <View style={styles.container}>
@@ -19,13 +57,13 @@ const ItemList = ({ items }) => {
                         styles.itemContainer,
                         selectedItemId === item.id && { backgroundColor: 'lightgray' },
                     ]}
-                    onPress={() => handleItemClick(item.id)}
+                    
                 >
                     <View style={styles.iconTitle}>
                         <AntDesign name={item.iconLeft} size={18} color="black" style={styles.icon} />
                         <Text style={styles.itemTitle}>{item.title}</Text>
                     </View>
-                    <FontAwesome6 name={item.iconRight} size={18} color="black" style={styles.icon} />
+                    <AntDesign name={item.iconRight} size={18} color="black" onPress={() => playSound(item)}/>
                 </TouchableOpacity>
             ))}
         </View>
